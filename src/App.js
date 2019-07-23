36dd62da3a7d4adbe09ad8aecf443ee9d8abe216
Varga Máté István <mateistvanvarga@gmail.com>
2019-07-23 17:02:31 +0200

    feat(Product): Product page.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import "antd/dist/antd.css";
+
+import Product from "./components/Product"
+
+function App() {
+  return (
+    <div className="App">
+      <Product></Product>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Product.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Button, Card, Select, Tabs, Typography } from 'antd';
+
+const { Option } = Select
+const { TabPane } = Tabs
+const { Title, Text } = Typography;
+
+function Product() {
+    const [selectedColor, setSelectedColor] = React.useState("black")
+    const [buttonText, setButtonText] = React.useState("ADD TO CART")
+    const [isLoadingButton, setIsLoadingButton] = React.useState(false)
+
+    const handleColorChange = value => {
+        setSelectedColor(value)
+    }
+
+    const addToCart = () => {
+        setIsLoadingButton(true)
+        setButtonText("LOADING...")
+
+        setTimeout(() => {
+            setIsLoadingButton(false)
+            setButtonText("VIEW CART")
+        }, 2000)
+    }
+
+    return (
+        <Card>
+            <div style={{
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <div style={{
+                    maxWidth: 500,
+                    float: "left"
+                }}>
+                    <Title level={1}>Audio-Technica ATH-MSR7</Title>
+                    <Text type="secondary">2017 Best Headphones of the Year Award Winner</Text>
+                    <Tabs 
+                        size="default"
+                        style={{
+                            minHeight: 250,
+                            marginTop: 25
+                        }}
+                    >
+                        <TabPane tab="DESCRIPTION" key="1">
+                            Springing from Audio-Technica’s rich heritage in professional audio, the ATH-MSR7 Over-Ear High-Resolution Audio Headphones are designed to reproduce Hi-Res Audio, allowing users to hear music the way it was intended. The over-ear headphones are outfitted with exclusive 45 mm True Motion Drivers, which utilize lightweight voice coils, a custom-mounted printed circuit board and specially designed diaphragm to improve transient response and minimize sound distortion for rich, detailed audio reproduction. 
+                        </TabPane>
+                        <TabPane tab="DETAIL" key="2">
+                            The ATH-MSR7 headphones also feature multi-layered air damping technology for extended mid-to-low frequency response. The housings, designed to “mirror” the full shape of the ear, are constructed of an aluminum/magnesium mix, layered to provide a lightweight, rigid structure that reduces unwanted resonance. Three precisely placed vents within these layers work to control air flow and improve dynamics.
+                        </TabPane>
+                    </Tabs>
+
+                    <div 
+                        style={{
+                            fontSize: 30
+                        }}
+                    >
+                        <Text strong>$59.99</Text>
+                        <Text strong delete type="secondary">$89.99</Text>
+                    </div>
+
+                    <Text
+                        strong
+                        style={{
+                            marginTop: 25,
+                            fontSize: 12
+                        }}
+                    >
+                        COLORS
+                    </Text>
+                    <br/>
+                    <Select
+                        defaultValue={selectedColor}
+                        onChange={handleColorChange}
+                        style={{
+                            marginBottom: 25
+                        }}
+                    >
+                        <Option value="black">Black</Option>
+                        <Option value="brown">Brown</Option>
+                    </Select>
+
+                    <br/>
+
+                    <Button 
+                        type="primary"
+                        loading={isLoadingButton}
+                        onClick={addToCart}
+                    >
+                        {buttonText}
+                    </Button>
+                </div>
+
+                <div style={{
+                    maxWidth: 500
+                }}>
+                    <img alt="product" src={`/images/ath-msr7-${selectedColor}.jpg`} width="500"/>
+                </div>
+            </div>
+        </Card>
+    )
+}
+
+export default Product
